454273c42b519e9c9b08d271d9387d896146bd12
Lucas da Cruz Neves <lucneves@gmail.com>
2023-07-11 17:44:02 -0300

    Adicionado dockerfile

---
+++ build-docker.sh
@@ -0,0 +1,19 @@
+cd "api-gateway-server"
+mvn clean package -DskipTests
+docker build -t gateway:1.0 .
+cd ..
+cd "config-server"
+mvn clean package -DskipTests
+docker build -t config:1.0 .
+cd ..
+cd "discovery-server"
+mvn clean package -DskipTests
+docker build -t discovery:1.0 .
+cd ..
+cd "company"
+mvn clean package -DskipTests
+docker build -t company:1.0 .
+cd ..
+cd "employee"
+mvn clean package -DskipTests
+docker build -t employee:1.0 .
